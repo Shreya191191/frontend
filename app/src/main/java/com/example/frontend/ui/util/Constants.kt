efe47a6f4e260c49317c446a567a2b01a307b97a
Shreya191191 <shreyatripathi359@gmail.com>
2026-06-27 02:36:54 +0530

    chore(config): add Constants for app-wide variables (Razorpay keys, platform fees)

---
+++ app/src/main/java/com/example/frontend/ui/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.frontend.ui.util
+
+object Constants {
+    const val RAZORPAY_KEY_ID = "rzp_test_T4lSpNSUdxwAqL"
+    const val DEFAULT_CURRENCY = "INR"
+    const val PLATFORM_FEE = 50.0
+    const val VALID_COUPON_CODE = "WELCOME50"
+    const val COUPON_DISCOUNT = 50.0
+}
